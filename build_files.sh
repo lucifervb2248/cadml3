f597b8533803358b0dc9e1d7be96836c47bd0f49
lucifervb2248 <117747477+lucifervb2248@users.noreply.github.com>
2024-07-19 16:53:12 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,11 @@
-python3 -m ensurepip --upgrade
-python3 -m pip install --upgrade pip
-python3.9 -m pip3.12 install --disable-pip-version-check --target . --upgrade -r /vercel/path0/requirements.txt
-python3.9 manage.py collectstatic
+set -e
+
+# Download and install pip for Python 3.12
+wget https://bootstrap.pypa.io/get-pip.py
+python3.12 get-pip.py
+
+# Activate the virtual environment (Windows)
+source venv/Scripts/activate
+
+python3.12 -m pip3.12 install --disable-pip-version-check --target . --upgrade -r /vercel/path0/requirements.txt
+python3.12 manage.py collectstatic
