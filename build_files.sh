bda006119ea772b5c00fe5ddfc4ce274fc0f63bc
lucifervb2248 <117747477+lucifervb2248@users.noreply.github.com>
2024-07-19 16:31:43 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,4 @@
+python3 -m ensurepip --upgrade
+python3 -m pip install --upgrade pip
 python3.9 -m pip3.12 install --disable-pip-version-check --target . --upgrade -r /vercel/path0/requirements.txt
 python3.9 manage.py collectstatic
