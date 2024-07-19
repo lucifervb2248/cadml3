b7e589e2ef2a236578192290cc5684bb3e9bec6a
lucifervb2248 <117747477+lucifervb2248@users.noreply.github.com>
2024-07-19 22:29:03 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -7,21 +7,5 @@ npm install -g vercel
 # Deploy with force to bypass cache
 vercel --force
 # Ensure the script exits if any command fails
-set -e
-
-# Use Python 3.9 (adjust according to the supported version on Vercel)
-PYTHON_VERSION="python3.9"
-
-# Create a virtual environment
-
-
-# Activate the virtual environment
-source venv/Scripts/activate
-
-# Upgrade pip
-pip install --upgrade pip
-
-# Install dependencies from requirements.txt
-pip install -r requirements.txt || { echo 'Failed to install dependencies'; exit 1; }
 
 python3.9 manage.py collectstatic
