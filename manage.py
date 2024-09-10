f44c06ee42f513b3a0e3501ff1ea63ebfd270efc
Vishnu B <117747477+lucifervb2248@users.noreply.github.com>
2024-09-10 12:36:50 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 from util.lwgk import LWGMKNN
 from sklearn.metrics.pairwise import euclidean_distances
-from django.core.management import execute_from_command_line
+#from django.core.management import execute_from_command_line
 venv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'venv', 'Lib', 'site-packages')
 
 # Add the virtual environment's site-packages to the system path
@@ -26,6 +26,16 @@ def main():
 
 
 if __name__ == '__main__':
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "We_CDSS.settings")  # Replace 'myproject' with your actual project name
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+
+    # Bind the app to the correct port
     port = os.getenv("PORT", "8000")
-    execute_from_command_line(["manage.py", "runserver", f"0.0.0.0:{port}"])
-    main()
+    execute_from_command_line([sys.argv[0], "runserver", f"0.0.0.0:{port}"])
