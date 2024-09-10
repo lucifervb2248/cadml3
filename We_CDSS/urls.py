4ac677a660624d87dbf9a63158a80b6aace918cd
Vishnu B <117747477+lucifervb2248@users.noreply.github.com>
2024-09-10 19:30:49 +0530

    Update urls.py

--- We_CDSS/urls.py
+++ We_CDSS/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('CDSSMain/',include('cdss.urls')),
+    path('',include('cdss.urls')),
     path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
